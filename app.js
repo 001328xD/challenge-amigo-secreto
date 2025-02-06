5facb0726d4521032a4179916b28400b95f4cf2b
Fran <fg_bw@hotmail.com>
2025-02-06 11:58:33 -0300

    actualiza app con funcion para capturar nombre del input

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaAmigos = []
-let esValido = ""
+let esInvalido = true
 
 function validarEntrada(dato){
     if (dato==""){
-        alert("Por favor ingrese un nombre")
-        esValido=false
-        console.log(esValido)
+        alert("Por favor ingrese un nombre");
     } else {
-        esValido=true
-        console.log(esValido)
+        esInvalido=true;
     }
 }
 
+function capturarNombre(){
+    nombre = document.getElementById("amigo").value;
+    return nombre;
+}
 
+function agregarAmigo(){
+    amigo=capturarNombre();
+    console.log(amigo)
+    console.log(typeof(amigo))
+    /*while (esInvalido) {
+        validarEntrada(amigo)
+    };
+    listaAmigos.push(amigo)
+    console.log(listaAmigos)*/
+}
