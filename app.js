f22d46ad327e730b4b8ceec495cc2da1262b5a84
Fran <fg_bw@hotmail.com>
2025-02-06 11:40:12 -0300

    actualiza app con funcion para validar si la entrada de nombre esta vacia

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaAmigos = []
+let esValido = ""
+
+function validarEntrada(dato){
+    if (dato==""){
+        alert("Por favor ingrese un nombre")
+        esValido=false
+        console.log(esValido)
+    } else {
+        esValido=true
+        console.log(esValido)
+    }
+}
+
+
